9f9b2166330ff5e21f3d596ad565d0b26f01b58d
Kieran Collins <kiecol@protonmail.com>
2023-02-10 23:47:54 +0000

    Render user information from GitHub

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,19 @@
+function userInformationHTML(user) {
+    return `
+    <h2>${user.name}
+        <span class="small-name">
+            (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+        </span>
+    </h2>
+    <div class="gh-content">
+        <div class="gh-avatar">
+            <a href="${user.html_url} target="_blank">
+                <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+        </div>
+        <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
+    </div>`
+}
+
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
@@ -11,13 +27,14 @@ function fetchGitHubInformation(event) {
             <img src="assets/images/loader.gif" alt="loading..." />
         </div>`);
 
-        $.when(
-            $.getJSON(`https://api.github.com/users/${username}`)
-        ).then(
-            function(response) {
-                var userData = response;
-                $("#gh-user-data").html(userInformationHTML(userData));
-        }, function(errorResponse) {
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function (response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        },
+        function (errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No information found for user ${username}</h2>`);
